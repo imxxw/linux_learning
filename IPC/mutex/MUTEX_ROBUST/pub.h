8be49052849c5fd14c27a3e2712c12561e105daa
xxw <imxxw@outlook.com>
2021-05-06 17:14:14 +0800

    [fix] fix bug of PPERR

--- IPC/mutex/MUTEX_ROBUST/pub.h
+++ IPC/mutex/MUTEX_ROBUST/pub.h
@@ -4,7 +4,7 @@
 #include <errno.h>
 
 #define PERR(msg) do { perror(msg); exit(-1); } while(0)
-#define PPERR(err, msg) do { err = errno; perror(msg); exit(-1); } while(0)
+#define PPERR(err, msg) do { errno = err; perror(msg); exit(-1); } while(0)
 
 struct ticket {
 	int remain;
